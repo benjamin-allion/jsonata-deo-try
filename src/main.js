4dc7197ef0d7af8b403266ac64b593f527684592
ALLION Benjamin <benjamin.allion.pro@gmail.com>
2020-08-13 23:58:47 +0200

    feat(i18n): add full i18n support. ref #7.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 import { Splitpanes, Pane } from 'splitpanes';
 import MonacoEditor from 'monaco-editor-vue';
 import 'splitpanes/dist/splitpanes.css';
+import messages from './i18n/messages';
 
 import App from './App.vue';
 
@@ -9,7 +11,16 @@ Vue.config.productionTip = false;
 Vue.component('splitpanes', Splitpanes);
 Vue.component('pane', Pane);
 Vue.component('monaco-editor', MonacoEditor);
+Vue.use(VueI18n);
+
+const locale = navigator.language;
+const i18n = new VueI18n({
+  locale,
+  fallbackLocale: 'en',
+  messages,
+});
 
 new Vue({
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
