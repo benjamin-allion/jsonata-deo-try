51452e7e575dc2b17cb64c72cb0aa2d1c91ac759
ALLION Benjamin <benjamin.allion.pro@gmail.com>
2020-08-13 17:12:54 +0200

    chore(lint): fix lint rules. ref #2.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-case-declarations': 1,
+    'no-underscore-dangle': 0
   },
 };
